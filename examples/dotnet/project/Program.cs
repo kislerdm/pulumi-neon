1a39521939933910cf091888cea15dd607e0a5ed
Dmitry Kisler <admin@dkisler.com>
2024-12-26 16:42:32 +0100

    add dotnet example for project creation

---
+++ examples/dotnet/project/Program.cs
@@ -0,0 +1,10 @@
+﻿using Pulumi;
+using PulumiSdk.Neon.Resource;
+
+return await Deployment.RunAsync(() =>
+{
+    new Project("myproject", new ProjectArgs
+    {
+        Name = "myProjectProvisionedWithPulumiDotnetSDK",
+    });
+});
